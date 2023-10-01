d8a3c531f09e5375aadd951f7f2ff403eddcd825
GHOST <ghostdevbusiness@gmail.com>
2023-10-01 22:38:09 +0000

    feat: better off quacko

--- src/lib/soundboard.ts
+++ src/lib/soundboard.ts
@@ -1,6 +1,10 @@
 import { join } from 'desm';
 
 export const SOUNDS = Object.freeze({
+	'better-off-quacko': join(
+		import.meta.url,
+		'./sounds/better-off-quacko.mp3',
+	),
 	'bruhdy-🦆': join(import.meta.url, './sounds/bruhdy-quack.mp3'),
 	'🦆': join(import.meta.url, './sounds/quack.mp3'),
 });
